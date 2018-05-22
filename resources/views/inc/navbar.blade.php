172c6c2d12029ba4269ae3bf0c52d181d23e7bbe
ZacharyToney <zach@clearseries.com>
2018-05-22 14:35:28 -0400

    initial commit halfway through laravel tutorial

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-inverse">
+  <div class="container">
+    <div class="navbar-header">
+      <button class="navbar-toggle collapsed" data-toggle="collapse">
+        <span class="sr-only">Toggle Navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="/" class="navbar-brand">{{config('app.name', 'LSAPP')}}</a>
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/services">Services</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
